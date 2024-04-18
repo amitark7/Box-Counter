f499596d366144367a1b39230a7f7ce7e7c90425
amitark7 <kumaramit21470@gmail.com>
2024-04-18 11:57:07 +0530

    Add padding in UI

--- src/component/CounterItem.js
+++ src/component/CounterItem.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const CounterItem = ({ counter, startCounter, stopCounter }) => {
   return (
-    <div className="text-white w-[80%] text-center mb-2 bg-black p-8 rounded-lg sm:w-[31%] md:w-[22%] lg:w-[18%]">
+    <div className="text-white w-[80%] text-center mb-2 bg-black p-3 py-5 rounded-lg sm:w-[31%] md:w-[22%] lg:w-[18%]">
       <button
-        className="border border-blue-400 rounded mb-2 text-base py-2 px-1 sm:text-sm md:text-xs"
+        className="border border-blue-400 rounded mb-2 text-base py-2 px-2"
         onClick={() =>
           counter.isStart ? stopCounter(counter) : startCounter(counter)
         }
